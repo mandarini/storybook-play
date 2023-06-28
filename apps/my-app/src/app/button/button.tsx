b347ac26046aa72fe63dec7931f40bc085a3bab7
Katerina Skroumpelou <sk.katherine@gmail.com>
2023-06-28 13:48:22 +0300

    new app

---
+++ apps/my-app/src/app/button/button.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+
+export function Button() {
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+
+  return <button onClick={handleClick}>You've clicked me {count} times</button>;
+}
+
+export default Button;
