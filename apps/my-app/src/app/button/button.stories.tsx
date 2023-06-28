6ce833e20b1ffdd14cbcffd3a790eed178599004
Katerina Skroumpelou <sk.katherine@gmail.com>
2023-06-28 13:50:56 +0300

    modified story

--- apps/my-app/src/app/button/button.stories.tsx
+++ apps/my-app/src/app/button/button.stories.tsx
@@ -1,12 +1,21 @@
-import type { Meta } from '@storybook/react';
+import type { Meta, StoryObj } from '@storybook/react';
 import { Button } from './button';
+import { within, userEvent } from '@storybook/testing-library';
+import { expect } from '@storybook/jest';
 
-const Story: Meta<typeof Button> = {
+const meta: Meta<typeof Button> = {
   component: Button,
   title: 'Button',
 };
-export default Story;
 
-export const Primary = {
-  args: {},
+export default meta;
+type Story = StoryObj<typeof Button>;
+
+export const ButtonClicked: Story = {
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const button = canvas.getByText("You've clicked me 0 times");
+    userEvent.click(button);
+    await expect(canvas.getByText("You've clicked me 1 times")).toBeDefined();
+  },
 };
